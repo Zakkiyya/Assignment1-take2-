e24800ca31b57170ca3d27f99a61d5741837ebca
u15128271 <47412891+u15128271@users.noreply.github.com>
2019-02-07 11:46:51 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,2 +1,4 @@
-document.getElementById("demo").style.fontSize = "35px";
-or
+function myFunction(){
+
+alert("More excited to broaden my programming knowledge and prepare myself for the working environment");
+}
