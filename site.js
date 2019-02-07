f3137aaaa8b03d80216b70fe84257b117f7443d9
Zakkiyya <47412878+Zakkiyya@users.noreply.github.com>
2019-02-07 11:46:48 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,2 +1,4 @@
-document.getElementById("demo").style.fontSize = "35px";
-or
+function myFunction(){
+
+alert("learning how to intergrate all the skills i have learnt over the past few years into a big assingment and becoming more prepared for the working environment");
+}
